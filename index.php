252634f1f5d5e5d5dbca18d621828be33e0f36ca
Sergii81 <sezik1@gmail.com>
2018-08-09 12:24:21 +0300

    files

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php
+	ini_set("display_errors", 1);	
+    Error_Reporting(E_ALL & ~E_NOTICE);
+
+    
+	
+	if (empty($_POST['name'])) {
+        
+            $answer=[
+                'status' => 'error',
+                'message' => 'Enter Name',
+                    ];
+           echo json_encode($answer);
+        } else { 
+        	$dsn = "mysql:host=localhost;dbname=testaweb;charset=utf8";
+			$opt = array (
+				    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+				    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		    );
+			$pdo = new PDO($dsn, 'root', '', $opt);
+			$user = $pdo->prepare('INSERT INTO users SET  name = ?, phone = ?');
+		    $user->execute(array($_POST['name'], $_POST['phone']));
+        	$answer= [
+		                'status' => 'added',
+		                'message' => 'Data is added',
+		                    ];
+		              echo json_encode($answer);
+		            }
+
+        	
 
 ?>
