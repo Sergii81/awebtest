3ea53419de753af6e3af4adf06441a04b0d918b8
Sergii81 <sezik1@gmail.com>
2018-08-10 10:58:06 +0300

    onsubmint

--- index.php
+++ index.php
@@ -11,24 +11,29 @@
                 'message' => 'Enter Name',
                     ];
            echo json_encode($answer);
-        } else { 
-        	$dsn = "mysql:host=localhost;dbname=testaweb;charset=utf8";
-			$opt = array (
-				    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-				    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-		    );
-			$pdo = new PDO($dsn, 'root', '', $opt);
-			$user = $pdo->prepare('INSERT INTO users SET  name = ?, phone = ?');
-		    $user->execute(array($_POST['name'], $_POST['phone']));
-        	if ($user){
-        		$answer= [
-		                'status' => 'added',
-		                'message' => 'Data is added',
-		                    ];
-		    echo json_encode($answer);
-        	}
+        } //else {
+        	if (isset($_POST['name']) && isset($_POST['phone'])){
+		    	$dsn = "mysql:host=localhost;dbname=testaweb;charset=utf8";
+				$opt = array (
+							PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+							PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+				);
+				$pdo = new PDO($dsn, 'root', '', $opt);
+		    	$user = $pdo->prepare('INSERT INTO users SET  name = ?, phone = ?');
+				$user->execute(array($_POST['name'], $_POST['phone']));
+		    } 	
+					
+		    if ($user){
+		        $answer= [
+				    'status' => 'added',
+				    'message' => 'Data is added',
+				          ];
+				echo json_encode($answer);
+		        	}
+		        //}
+    
         	
-		            }
+		            
 
         	
 
